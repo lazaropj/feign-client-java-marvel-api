f736eb1be03dc5edc6d84634d23ddb09f6ce52ba
Lazaro Jr <linkout.lazaro@gmail.com>
2022-04-21 18:33:46 -0300

    first commit

---
+++ src/main/java/io/swagger/model/ComicList.java
@@ -0,0 +1,167 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.model.ComicSummary;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * ComicList
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-04-21T18:29:03.913563-03:00[America/Sao_Paulo]")
+
+
+public class ComicList   {
+  @JsonProperty("available")
+  private BigDecimal available = null;
+
+  @JsonProperty("returned")
+  private BigDecimal returned = null;
+
+  @JsonProperty("collectionURI")
+  private String collectionURI = null;
+
+  @JsonProperty("items")
+  @Valid
+  private List<ComicSummary> items = null;
+
+  public ComicList available(BigDecimal available) {
+    this.available = available;
+    return this;
+  }
+
+  /**
+   * The number of total available issues in this list. Will always be greater than or equal to the \"returned\" value.
+   * @return available
+   **/
+  @ApiModelProperty(value = "The number of total available issues in this list. Will always be greater than or equal to the \"returned\" value.")
+  
+    @Valid
+    public BigDecimal getAvailable() {
+    return available;
+  }
+
+  public void setAvailable(BigDecimal available) {
+    this.available = available;
+  }
+
+  public ComicList returned(BigDecimal returned) {
+    this.returned = returned;
+    return this;
+  }
+
+  /**
+   * The number of issues returned in this collection (up to 20).
+   * @return returned
+   **/
+  @ApiModelProperty(value = "The number of issues returned in this collection (up to 20).")
+  
+    @Valid
+    public BigDecimal getReturned() {
+    return returned;
+  }
+
+  public void setReturned(BigDecimal returned) {
+    this.returned = returned;
+  }
+
+  public ComicList collectionURI(String collectionURI) {
+    this.collectionURI = collectionURI;
+    return this;
+  }
+
+  /**
+   * The path to the full list of issues in this collection.
+   * @return collectionURI
+   **/
+  @ApiModelProperty(value = "The path to the full list of issues in this collection.")
+  
+    public String getCollectionURI() {
+    return collectionURI;
+  }
+
+  public void setCollectionURI(String collectionURI) {
+    this.collectionURI = collectionURI;
+  }
+
+  public ComicList items(List<ComicSummary> items) {
+    this.items = items;
+    return this;
+  }
+
+  public ComicList addItemsItem(ComicSummary itemsItem) {
+    if (this.items == null) {
+      this.items = new ArrayList<ComicSummary>();
+    }
+    this.items.add(itemsItem);
+    return this;
+  }
+
+  /**
+   * The list of returned issues in this collection.
+   * @return items
+   **/
+  @ApiModelProperty(value = "The list of returned issues in this collection.")
+      @Valid
+    public List<ComicSummary> getItems() {
+    return items;
+  }
+
+  public void setItems(List<ComicSummary> items) {
+    this.items = items;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    ComicList comicList = (ComicList) o;
+    return Objects.equals(this.available, comicList.available) &&
+        Objects.equals(this.returned, comicList.returned) &&
+        Objects.equals(this.collectionURI, comicList.collectionURI) &&
+        Objects.equals(this.items, comicList.items);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(available, returned, collectionURI, items);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class ComicList {\n");
+    
+    sb.append("    available: ").append(toIndentedString(available)).append("\n");
+    sb.append("    returned: ").append(toIndentedString(returned)).append("\n");
+    sb.append("    collectionURI: ").append(toIndentedString(collectionURI)).append("\n");
+    sb.append("    items: ").append(toIndentedString(items)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
---
+++ src/main/java/io/swagger/model/SeriesDataContainer.java
@@ -0,0 +1,193 @@
+package io.swagger.model;
+
+import java.util.Objects;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.model.Series;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.validation.annotation.Validated;
+import javax.validation.Valid;
+import javax.validation.constraints.*;
+
+/**
+ * SeriesDataContainer
+ */
+@Validated
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2022-04-21T18:29:03.913563-03:00[America/Sao_Paulo]")
+
+
+public class SeriesDataContainer   {
+  @JsonProperty("offset")
+  private BigDecimal offset = null;
+
+  @JsonProperty("limit")
+  private BigDecimal limit = null;
+
+  @JsonProperty("total")
+  private BigDecimal total = null;
+
+  @JsonProperty("count")
+  private BigDecimal count = null;
+
+  @JsonProperty("results")
+  @Valid
+  private List<Series> results = null;
+
+  public SeriesDataContainer offset(BigDecimal offset) {
+    this.offset = offset;
+    return this;
+  }
+
+  /**
+   * The requested offset (number of skipped results) of the call.
+   * @return offset
+   **/
+  @ApiModelProperty(value = "The requested offset (number of skipped results) of the call.")
+  
+    @Valid
+    public BigDecimal getOffset() {
+    return offset;
+  }
+
+  public void setOffset(BigDecimal offset) {
+    this.offset = offset;
+  }
+
+  public SeriesDataContainer limit(BigDecimal limit) {
+    this.limit = limit;
+    return this;
+  }
+
+  /**
+   * The requested result limit.
+   * @return limit
+   **/
+  @ApiModelProperty(value = "The requested result limit.")
+  
+    @Valid
+    public BigDecimal getLimit() {
+    return limit;
+  }
+
+  public void setLimit(BigDecimal limit) {
+    this.limit = limit;
+  }
+
+  public SeriesDataContainer total(BigDecimal total) {
+    this.total = total;
+    return this;
+  }
+
+  /**
+   * The total number of resources available given the current filter set.
+   * @return total
+   **/
+  @ApiModelProperty(value = "The total number of resources available given the current filter set.")
+  
+    @Valid
+    public BigDecimal getTotal() {
+    return total;
+  }
+
+  public void setTotal(BigDecimal total) {
+    this.total = total;
+  }
+
+  public SeriesDataContainer count(BigDecimal count) {
+    this.count = count;
+    return this;
+  }
+
+  /**
+   * The total number of results returned by this call.
+   * @return count
+   **/
+  @ApiModelProperty(value = "The total number of results returned by this call.")
+  
+    @Valid
+    public BigDecimal getCount() {
+    return count;
+  }
+
+  public void setCount(BigDecimal count) {
+    this.count = count;
+  }
+
+  public SeriesDataContainer results(List<Series> results) {
+    this.results = results;
+    return this;
+  }
+
+  public SeriesDataContainer addResultsItem(Series resultsItem) {
+    if (this.results == null) {
+      this.results = new ArrayList<Series>();
+    }
+    this.results.add(resultsItem);
+    return this;
+  }
+
+  /**
+   * The list of series returned by the call
+   * @return results
+   **/
+  @ApiModelProperty(value = "The list of series returned by the call")
+      @Valid
+    public List<Series> getResults() {
+    return results;
+  }
+
+  public void setResults(List<Series> results) {
+    this.results = results;
+  }
+
+
+  @Override
+  public boolean equals(java.lang.Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    SeriesDataContainer seriesDataContainer = (SeriesDataContainer) o;
+    return Objects.equals(this.offset, seriesDataContainer.offset) &&
+        Objects.equals(this.limit, seriesDataContainer.limit) &&
+        Objects.equals(this.total, seriesDataContainer.total) &&
+        Objects.equals(this.count, seriesDataContainer.count) &&
+        Objects.equals(this.results, seriesDataContainer.results);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(offset, limit, total, count, results);
+  }
+
+  @Override
+  public String toString() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("class SeriesDataContainer {\n");
+    
+    sb.append("    offset: ").append(toIndentedString(offset)).append("\n");
+    sb.append("    limit: ").append(toIndentedString(limit)).append("\n");
+    sb.append("    total: ").append(toIndentedString(total)).append("\n");
+    sb.append("    count: ").append(toIndentedString(count)).append("\n");
+    sb.append("    results: ").append(toIndentedString(results)).append("\n");
+    sb.append("}");
+    return sb.toString();
+  }
+
+  /**
+   * Convert the given object to string with each line indented by 4 spaces
+   * (except the first line).
+   */
+  private String toIndentedString(java.lang.Object o) {
+    if (o == null) {
+      return "null";
+    }
+    return o.toString().replace("\n", "\n    ");
+  }
+}
